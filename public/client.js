fd16d28795d0e13ccce40437dbeeb992ead0aa92
jangerine <seaboylove7777@gmail.com>
2026-09-15 15:14:58 +0900

    Create client.js

---
+++ public/client.js
@@ -0,0 +1,72 @@
+const socket = io();
+
+let currentRoomId = '';
+let myId = '';
+
+socket.on('connect', () => {
+  myId = socket.id;
+});
+
+function joinGame() {
+  const username = document.getElementById('username').value;
+  const roomId = document.getElementById('room-id').value;
+
+  if (!username || !roomId) {
+    alert('닉네임과 방 코드를 입력하세요.');
+    return;
+  }
+
+  currentRoomId = roomId;
+  socket.emit('joinRoom', { roomId, username });
+
+  document.getElementById('login-sec').style.display = 'none';
+  document.getElementById('game-sec').style.display = 'block';
+  document.getElementById('display-room').innerText = roomId;
+}
+
+function drawCard() {
+  socket.emit('drawCard', { roomId: currentRoomId });
+}
+
+function shoutMuffinTime() {
+  socket.emit('shoutMuffinTime', { roomId: currentRoomId });
+}
+
+// 서버로부터 방 상태 업데이트 수신
+socket.on('updateRoom', (room) => {
+  // 플레이어 리스트 갱신
+  const playerList = document.getElementById('player-list');
+  playerList.innerHTML = '';
+  
+  room.players.forEach((p, index) => {
+    const isMyTurn = index === room.turnIndex;
+    const li = document.createElement('li');
+    li.innerText = `${p.name} (카드 ${p.hand.length}장) ${p.isMuffinTime ? '🧁[머핀 타임!]' : ''} ${isMyTurn ? '👈 현재 턴' : ''}`;
+    playerList.appendChild(li);
+  });
+
+  // 내 카드 갱신
+  const me = room.players.find(p => p.id === socket.id);
+  if (me) {
+    document.getElementById('hand-count').innerText = me.hand.length;
+    const handDiv = document.getElementById('my-hand');
+    handDiv.innerHTML = '';
+
+    me.hand.forEach(card => {
+      const cardEl = document.createElement('div');
+      cardEl.className = `card ${card.type}`;
+      cardEl.innerHTML = `<strong>${card.name}</strong><br><small>${card.desc}</small>`;
+      handDiv.appendChild(cardEl);
+    });
+  }
+});
+
+// 로그 출력
+socket.on('log', (msg) => {
+  const logBox = document.getElementById('log-box');
+  const p = document.createElement('p');
+  p.style.margin = "2px 0";
+  p.innerText = msg;
+  logBox.appendChild(p);
+  logBox.scrollTop = logBox.scrollHeight;
+});
